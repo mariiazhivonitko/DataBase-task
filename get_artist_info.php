2900b00ddebe54602e62719e21888cfebb010754
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2023-04-24 10:07:39 +0300

    add get_artist_info.php

---
+++ get_artist_info.php
@@ -0,0 +1,33 @@
+<?php
+require "dbconnection.php";
+$dbcon = createDbConnection();
+
+$artist_id=1;
+
+$sql = "SELECT Name FROM artists WHERE ArtistID=$artist_id";
+$statement = $dbcon->prepare($sql);
+$statement -> execute();
+$artist=$statement;
+
+
+$sql="SELECT Title
+FROM albums
+WHERE ArtistID=$artist_id";
+$statement = $dbcon->prepare($sql);
+$statement -> execute();
+$title=$statement;
+
+$sql="SELECT Name
+FROM tracks, albums
+WHERE albums.AlbumId = tracks.AlbumId AND Title=$title";
+$statement = $dbcon->prepare($sql);
+$statement -> execute();
+$tracks=$statement;
+
+$albums = array("title"=> $title, "tracks"=> $tracks);
+
+$response = array("artist"=>$artist, "albums"=>$albums);
+
+$json = json_encode($response);
+header('Content-type: application/json');
+echo $json;
