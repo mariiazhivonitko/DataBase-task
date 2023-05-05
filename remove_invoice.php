fbf15223435b7eee2a77e5c9d4b4b66ef5baff47
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2023-05-05 13:39:26 +0300

    checked remove_invoice

--- remove_invoice.php
+++ remove_invoice.php
@@ -2,7 +2,7 @@
 require "dbconnection.php";
 $dbcon = createDbConnection();
 
-$invoice_item_id = strip_tags($_POST["invoice_item_id"]);
+$invoice_item_id = 10;
  
 $sql = "DELETE FROM invoice_items WHERE invoiceLineId=$invoice_item_id";
 $dbcon->exec($sql);
